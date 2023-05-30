b5fb72236f6848c086bd2b553c973629e6326ec9
MostafaSaidGad <131365332+MostafaSaidGad@users.noreply.github.com>
2023-05-31 00:32:02 +0200

    final update

--- TrainPro/Program.cs
+++ TrainPro/Program.cs
@@ -49,13 +49,13 @@ namespace ImageProcessing
                     Cv2.DrawContours(pic2, new OpenCvSharp.Point[][] { contours[i] }, -1, Scalar.Blue, 1);
                     Good_Contour_Ref.Add(i);
                     Good_Contour++;
-                    Cv2.ImShow("Contours after first filtration", pic2);
+                    Cv2.ImShow("Contours after filtration", pic2);
 
                     // Create a mask for the contour
                     Mat Contour_Mask = Mat.Zeros(pic.Rows, pic.Cols, MatType.CV_8UC1);
                     Cv2.DrawContours(Contour_Mask, contours, i, 255, -1);
 
-                    // Compute the mean gray value inside the contour
+                    // Calculate the mean gray value inside the contour
                     Scalar meanGray = Cv2.Mean(pic, Contour_Mask);
                     double grayMeanValue = meanGray.Val0;
                     GrayMean.Add(grayMeanValue);
@@ -88,11 +88,11 @@ namespace ImageProcessing
                     Mat contourMask = Mat.Zeros(pic.Rows, pic.Cols, MatType.CV_8UC1);
                     Cv2.DrawContours(contourMask, contours, i, 255, -1);
 
-                    // Compute the mean gray value inside the contour
+                    // Compute the mean gray value inside the contours
                     Scalar meanGray = Cv2.Mean(pic, contourMask);
                     double grayMeanValue = meanGray.Val0;
 
-                    // Check if the gray mean value is below the cutoff value
+                    // Check if the gray mean value is less than the cutoff value
                     if (grayMeanValue < cutoff)
                     {
                         Cv2.DrawContours(pic2, contours, i, Scalar.Green, Cv2.FILLED);
@@ -113,7 +113,7 @@ namespace ImageProcessing
             Console.Write("----------------------------------------------------------------\n");
 
 
-            // APROXIMATEPOLYDP DRAW
+            // detecting corner points using APROXIMATEPOLYDP algorithm
             //double Epsilon = 0.5;
             double Epsilon = 0.03;
             //double Epsilon = 0.1;
@@ -133,7 +133,7 @@ namespace ImageProcessing
             }
             Cv2.ImShow("PicWithNonFilteredPoints", pic2);
 
-            /// filtering out closed double points
+            /// filtering out close corner points
             OpenCvSharp.Point[][] FilteredPoints = NotFilteredPoints;
             /////////////////close points filtration constant////////////
             //double filtrationconstant = 0.05;
@@ -150,7 +150,7 @@ namespace ImageProcessing
                     {
                         double distance_between_two_points = Math.Sqrt(Math.Pow((FilteredPoints[k][i].X - FilteredPoints[k][j].X), 2) + Math.Pow((FilteredPoints[k][i].Y - FilteredPoints[k][j].Y), 2));
 
-                        if (distance_between_two_points < filtration_constant * Cv2.ArcLength(contours[goodQuadReferences[k]], true))
+                        if (distance_between_two_points < filtration_constant * Cv2.ArcLength(contours[goodQuadReferences[k]], true)) 
                         {
                             int midx = (FilteredPoints[k][i].X + FilteredPoints[k][j].X) / 2;
                             int midy = (FilteredPoints[k][i].Y + FilteredPoints[k][j].Y) / 2;
@@ -169,19 +169,19 @@ namespace ImageProcessing
             {
                 for (int i = 0; i < FilteredPoints[k].Length; i++)
                 {
-                    Cv2.Circle(pic2, FilteredPoints[k][i].X, FilteredPoints[k][i].Y, 1, Scalar.Black, 2);
+                    Cv2.Circle(pic2, FilteredPoints[k][i].X, FilteredPoints[k][i].Y, 1, Scalar.Black, 2); //drawing fitted points between the close points
                     //Debug.WriteLine(FilteredPoints[k][i].X); Debug.WriteLine(FilteredPoints[k][i].Y);
                 }
                  if (FilteredPoints[k].Length > 2)
                  {
                      for (int i = 0; i < FilteredPoints[k].Length - 1; i++)
                      {
-                         Cv2.Line(pic2, FilteredPoints[k][i], FilteredPoints[k][i + 1], Scalar.Black, 1);
+                         Cv2.Line(pic2, FilteredPoints[k][i], FilteredPoints[k][i + 1], Scalar.Black, 1); //Drawing fitted lines between the points of the same contour
                      }
                  }
             }
 
-                Cv2.ImShow("PicWithFilteredPoints", pic2);
+                Cv2.ImShow("Filtered Corner Points + Ftted Lines", pic2);
 
 
                 // Create a HashSet<Point> to store unique points
